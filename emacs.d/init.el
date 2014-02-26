5f1874d897b9c295be64e5b2de43b202cdf4cb21
Craig Buchek <craig@boochtek.com>
2014-02-25 20:22:04 -0600

    Add Emacs init file, which includes Prelude.

---
+++ emacs.d/init.el
@@ -0,0 +1,25 @@
+;;; Emacs Custom Configuration.
+
+;; Load Prelude.
+(load "~/.emacs.d/prelude/init.el")
+
+;; Configure Prelude.
+(setq prelude-guru nil) ;; Don't disable cursor keys.
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-c)
+(require 'prelude-coffee)
+(require 'prelude-css)
+(require 'prelude-emacs-lisp)
+(require 'prelude-js)
+(require 'prelude-key-chord) ;; Binds useful features to key combinations
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-python)
+(require 'prelude-ruby)
+(require 'prelude-scala)
+(require 'prelude-scss)
+(require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
+
+;; Store customize UI changes in .emacs.d/custom.el. (Revert Prelude's change.)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
