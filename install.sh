96e9e470cbe8bb9fe0f94de9e26738537094e549
Craig Buchek <craig@boochtek.com>
2014-02-25 20:00:07 -0600

    Use symbolic links instead of hard links.

--- install.sh
+++ install.sh
@@ -23,43 +23,43 @@ TODAY=`date +'%Y%m%d'`
 #fi
 
 # Link files to where they belong.
-ln -f ackrc             ~/.ackrc
+ln -sf ackrc             ~/.ackrc
 
-ln -f bash_aliases      ~/.bash_aliases
-ln -f bash_logout       ~/.bash_logout
-ln -f bash_profile      ~/.bash_profile
-ln -f bashrc            ~/.bashrc
-ln -f profile           ~/.profile
+ln -sf bash_aliases      ~/.bash_aliases
+ln -sf bash_logout       ~/.bash_logout
+ln -sf bash_profile      ~/.bash_profile
+ln -sf bashrc            ~/.bashrc
+ln -sf profile           ~/.profile
 
 mkdir -p ~/.bundle
-ln -f bundle/config     ~/.bundle/config
-ln -f gemrc             ~/.gemrc
-ln -f irbrc             ~/.irbrc
-ln -f aprc              ~/.aprc
-ln -f railsrc           ~/.railsrc
+ln -sf bundle/config     ~/.bundle/config
+ln -sf gemrc             ~/.gemrc
+ln -sf irbrc             ~/.irbrc
+ln -sf aprc              ~/.aprc
+ln -sf railsrc           ~/.railsrc
 
-ln -f gitconfig         ~/.gitconfig
-ln -f gitignore         ~/.gitignore
+ln -sf gitconfig         ~/.gitconfig
+ln -sf gitignore         ~/.gitignore
 
-ln -f inputrc           ~/.inputrc
+ln -sf inputrc           ~/.inputrc
 
 mkdir -p ~/.mc
-ln -f mc/ini            ~/.mc/ini
+ln -sf mc/ini            ~/.mc/ini
 
-ln -f nanorc            ~/.nanorc
+ln -sf nanorc            ~/.nanorc
 mkdir -p ~/.nano
-ln -f nano/css.nanorc   ~/.nano/css.nanorc
-ln -f nano/php.nanorc   ~/.nano/php.nanorc
-ln -f nano/xml.nanorc   ~/.nano/xml.nanorc
+ln -sf nano/css.nanorc   ~/.nano/css.nanorc
+ln -sf nano/php.nanorc   ~/.nano/php.nanorc
+ln -sf nano/xml.nanorc   ~/.nano/xml.nanorc
 
-ln -f vimrc             ~/.vimrc
+ln -sf vimrc             ~/.vimrc
 mkdir -p ~/.vim
 mkdir -p ~/.vim/backup  # Make sure there's a global backup directory for vim.
-ln -f vim/packages.vim  ~/.vim/packages.vim
-ln -f vim/abbrev.vim    ~/.vim/abbrev.vim
-ln -f vim/keymaps.vim   ~/.vim/keymaps.vim
+ln -sf vim/packages.vim  ~/.vim/packages.vim
+ln -sf vim/abbrev.vim    ~/.vim/abbrev.vim
+ln -sf vim/keymaps.vim   ~/.vim/keymaps.vim
 
-ln -f psqlrc            ~/.psqlrc
+ln -sf psqlrc            ~/.psqlrc
 
 if [ "`uname`"x = "Darwin"x  ]; then
   mkdir -p ~/Library/KeyBindings
