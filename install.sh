4c5297a890cc732b4f19ed7c83326c824ec3f864
Craig Buchek <craig@boochtek.com>
2017-06-25 15:05:24 -0500

    Add config for Karabiner Elements
    * Don't show in menu bar
    * Space bar with both Shift keys toggles Caps Lock
    * Caps Lock works as Control, or Escape if no other key pressed
    * Fn+Tab and Fn+Shift+Tab works as Ctrl+Tab and Ctrl+Shift+Tab

--- install.sh
+++ install.sh
@@ -82,6 +82,7 @@ if [ "$(uname)"x = "Darwin"x  ]; then
   ln -f DefaultKeyBinding.Dict ~/Library/KeyBindings/DefaultKeyBinding.Dict
 
   ln -sf "$HOME/config_files/karabiner.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
+  ln -sf "$HOME/config_files/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
 
   ln -sf "$HOME/config_files/sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
   ln -sf "$HOME/config_files/sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
