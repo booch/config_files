14f426abba6d6a280a5a49ad419530d88a059c61
Craig Buchek <craig@boochtek.com>
2017-08-09 10:11:47 -0500

    Remove Sublime Text configs
    We're using Atom now, and don't have a use for Sublime Text any more.

--- install.sh
+++ install.sh
@@ -83,11 +83,6 @@ if [ "$(uname)"x = "Darwin"x  ]; then
 
   ln -sf "$HOME/config_files/karabiner.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
   ln -sf "$HOME/config_files/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
-
-  ln -sf "$HOME/config_files/sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-  ln -sf "$HOME/config_files/sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
-  ln -sf "$HOME/config_files/sublime/Ruby.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Ruby.sublime-settings"
-  # ln -sf "$HOME/config_files/sublime/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
 fi
 
 
