d187bdc68b7017bab4800995ce82b977dfc6f3aa
Craig Buchek <c.buchek@f5.com>
2017-08-26 17:20:24 -0500

    Chruby: Run immediately, if necessary
    Idea from https://github.com/grassdog/dotfiles/blob/063a352767e837115ece59b00fea40e6a7499278/zshenv#L48-L49

--- profile.d/chruby.sh
+++ profile.d/chruby.sh
@@ -1,5 +1,11 @@
-# Automatically change Ruby versions when changing directories.
 if [[ -d /usr/local/share/chruby ]]; then
+    # We have to source this, because it's implemented as a shell function.
     source /usr/local/share/chruby/chruby.sh
+
+    # Automatically change Ruby versions when changing directories.
     source /usr/local/share/chruby/auto.sh
+
+    # See if we need to change Ruby versions for the directory we're in now.
+    [[ -r ~/.ruby-version ]] && chruby $(cat ~/.ruby-version)
+    [[ -r ./.ruby-version ]] && chruby $(cat ./.ruby-version)
 fi
