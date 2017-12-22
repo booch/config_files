966a5b435b7c2e359600454933204fae141eefc6
Craig Buchek <c.buchek@f5.com>
2017-12-21 23:45:19 -0600

    Bash: add aliases for verboten commands

---
+++ profile.d/verboten_commands.sh
@@ -0,0 +1,8 @@
+# Prevent myself from using some commands that I don't want to use anymore.
+
+alias nslookup='echo "Use dig!"'
+alias traceroute='echo "Use mtr!"'
+alias nano='echo "Use vim!"'
+alias pico='echo "Use vim!"'
+alias ack='echo "Use rg!"'
+alias ag='echo "Use rg!"'
