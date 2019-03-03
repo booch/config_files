e7b53efbdf0b57732c83568224da956b594b5a16
Craig Buchek <craig@boochtek.com>
2019-03-02 21:21:10 -0600

    Add spelling dictionary

--- install.sh
+++ install.sh
@@ -50,6 +50,8 @@ ln -sf config_files/gitconfig         ~/.gitconfig
 ln -sf config_files/gitignore         ~/.gitignore
 ln -sf config_files/gitattributes     ~/.gitattributes
 
+ln -sf config_files/spelling/dictionary.txt ~/Library/Spelling/LocalDictionary
+
 ln -sf config_files/inputrc           ~/.inputrc
 
 ln -sf config_files/ctags             ~/.ctags
