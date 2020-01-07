d408e0e738493d264343195cb3b92cc17a22ae4b
Craig Buchek <craig@boochtek.com>
2020-01-06 20:44:02 -0600

    ZSH: Make Home/End keys work in iTerm2

--- zsh/zshrc.d/key_bindings.zsh
+++ zsh/zshrc.d/key_bindings.zsh
@@ -1,2 +1,6 @@
 # Use Emacs-style keybindings.
 bindkey -e
+
+# These *had* been working in iTerm2, but then seemed to have stopped working.
+bindkey '\e[1~' beginning-of-line       # Home
+bindkey '\e[4~' end-of-line             # End
