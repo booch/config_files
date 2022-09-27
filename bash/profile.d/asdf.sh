b2bd6d9e76b19c13e904b4610346fcd0d356bb3c
Craig Buchek <craig@boochtek.com>
2022-09-27 13:03:53 -0500

    ASDF: Ensure it works for ZSH and BASH

--- bash/profile.d/asdf.sh
+++ bash/profile.d/asdf.sh
@@ -1,4 +1,1 @@
-if [[ -d $(brew --prefix asdf) ]]; then
-    # We have to source this, because it's implemented as a shell function.
-    source $(brew --prefix asdf)/asdf.sh
-fi
+../../zsh/zshrc.d/asdf.zsh
