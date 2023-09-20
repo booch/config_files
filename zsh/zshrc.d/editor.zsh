fe9113fd90fd93a275f7335f5d4a04ab468ddede
Craig Buchek <craig@boochtek.com>
2023-09-20 12:04:06 -0500

    ZSH / Bash: Default to nvim, with fallbacks

--- zsh/zshrc.d/editor.zsh
+++ zsh/zshrc.d/editor.zsh
@@ -1,12 +1,26 @@
-# Set the default editor to vim, if it exists. Next best choice is nano.
-if type -p vim >/dev/null; then
+# Set the default editor to nvim, if it exists. Fall back to vim, nano, or pico.
+if type -p nvim >/dev/null; then
+    export EDITOR=nvim
+    alias vi=nvim
+    alias vim=nvim
+    alias nano=nvim
+    alias pico=nvim
+elif type -p vim >/dev/null; then
     export EDITOR=vim
+    alias vi=vim
+    alias nvim=vim
     alias nano=vim
     alias pico=vim
-elif type -p nano >/dev/null; then
+elif type -P nano >/dev/null; then
     export EDITOR=nano
+    alias vi=nano
+    alias vim=nano
+    alias nvim=nano
     alias pico=nano
-elif type -p pico >/dev/null; then
+elif type -P pico >/dev/null; then
     export EDITOR=pico
+    alias vi=pico
+    alias vim=pico
+    alias nvim=pico
     alias nano=pico
 fi
