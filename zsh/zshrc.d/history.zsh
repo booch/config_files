48d11fdc91cd641ed65397c5540630f2278caaad
Craig Buchek <craig@boochtek.com>
2023-10-01 13:19:10 -0500

    ZSH: Configure shell history
    From https://gitlab.com/sytses/dotfiles/-/blob/master/zsh/config.zsh

--- zsh/zshrc.d/history.zsh
+++ zsh/zshrc.d/history.zsh
@@ -7,3 +7,12 @@ HISTFILE=$HOME/.zsh/history
 # Allow for a really large history file.
 HISTSIZE=20000
 SAVEHIST=10000
+
+# From https://gitlab.com/sytses/dotfiles/-/blob/master/zsh/config.zsh
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions ???
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
