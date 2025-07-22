f137488b1cd2bcbdb4d3f0949a37e2d182ce07e9
Craig Buchek <craig@boochtek.com>
2025-07-22 11:10:28 -0700

    Zsh: use mise instead of ASDF
    My mise script wasn't running, because it needs to have a `zsh`
    extension. The ASDF script was actually doing the mise setup.
    Also ensure silent execution.

--- zsh/zshrc.d/asdf.sh
+++
@@ -1,6 +0,0 @@
-# #!/bin/bash
-
-# if [[ -d "$(brew --prefix asdf)" ]]; then
-#     # We have to source this, because it's implemented as a shell function.
-#     source "$(brew --prefix asdf)/libexec/asdf.sh"
-# fi
--- zsh/zshrc.d/mise.zsh
+++ zsh/zshrc.d/mise.zsh
@@ -4,9 +4,9 @@
 export MISE_RUBY_DEFAULT_PACKAGES_FILE="$HOME/.config/ruby/default-gems"
 
 # Activate Mise - adds a shell function (so `mise shell` works) and a precmd function (to change $PATH).
-if command -v mise &>/dev/null; then
-    eval "$(mise activate)"
+if command -v mise &> /dev/null; then
+    eval "$(mise activate)" &> /dev/null
 fi
 
 # Load Mise completions.
-eval "$(mise completion $(basename $SHELL))"
+eval "$(mise completion $(basename $SHELL))" &> /dev/null
