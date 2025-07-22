c0064d0aa59646aafad09a07801c41138ab17459
Craig Buchek <craig@boochtek.com>
2025-07-22 11:18:15 -0700

    Zsh: update user startup script
    - Get full name from OS
    - Set PayNearMe email address, based on hostname

--- zsh/zshrc.d/user.zsh
+++ zsh/zshrc.d/user.zsh
@@ -1,9 +1,20 @@
 # Set some user-specific environment variables for ease of use.
 
-# LOL. Copilot suggested 'Booch McBoochface' as the full name.
-if [[ "$USER" == 'booch' || "$USER" == 'craigbuchek' ]]; then
-    export USER_FULL_NAME='Craig Buchek'
+
+local THIS_FILE="${(%):-%N}"
+
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    export USER_FULL_NAME="$(id -F)"
+else
+    export USER_FULL_NAME="$(getent passwd "$USER" | cut -d: -f5 | cut -d, -f1)"
+fi
+
+
+if [[ "$HOST" == 'Buchek-FNWWVWCRWN' ]]; then
+    export USER_EMAIL_ADDRESS="$USER@paynearme.com"
+elif [[ "$USER" == 'booch' || "$USER" == 'craigbuchek' ]]; then
     export USER_EMAIL_ADDRESS='craig@boochtek.com'
 else
-    echo 'WARNING: Unknown user. Please update `zshrc.d/user.zsh`.'
+    echo "WARNING: Unknown user. Please set USER_EMAIL_ADDRESS in '$THIS_FILE'."
 fi
