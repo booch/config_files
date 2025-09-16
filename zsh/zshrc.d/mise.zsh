bfd8d07ddeef6cfe7da2e57d24d16896bac64af2
Craig Buchek <craig@boochtek.com>
2025-09-15 23:17:47 -0700

    Don't try to add mise completions if there is no mise

--- zsh/zshrc.d/mise.zsh
+++ zsh/zshrc.d/mise.zsh
@@ -6,7 +6,7 @@ export MISE_RUBY_DEFAULT_PACKAGES_FILE="$HOME/.config/ruby/default-gems"
 # Activate Mise - adds a shell function (so `mise shell` works) and a precmd function (to change $PATH).
 if command -v mise &> /dev/null; then
     eval "$(mise activate)" &> /dev/null
-fi
 
-# Load Mise completions.
-eval "$(mise completion $(basename $SHELL))" &> /dev/null
+    # Load Mise completions.
+    eval "$(mise completion $(basename $SHELL))" &> /dev/null
+fi
