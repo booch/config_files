95ea3e3895d7fedcdd8847e8f26889e519d81f6d
Craig Buchek <craig@boochtek.com>
2025-09-18 20:57:31 -0700

    Zshrc: Only load ssh-agent if it is available
    It's not available on my OpenWRT router.

--- zsh/zshrc.d/ssh.zsh
+++ zsh/zshrc.d/ssh.zsh
@@ -2,7 +2,14 @@
 #!/bin/zsh
 # This should be able to run in both Bash and Zsh.
 
+
 PID_FILE="${XDG_STATE_HOME:-$HOME/.ssh}/ssh-agent.pid"
+
+
+# Exit if we don't have ssh-agent binary available.
+if ! command -v ssh-agent > /dev/null; then
+    return 0
+fi
 
 # Remove the PID file if it's no longer valid.
 if [[ -f "${PID_FILE}" ]] && ! pgrep -x ssh-agent -F "${PID_FILE}" > /dev/null; then
@@ -11,7 +18,7 @@ fi
 
 # Start ssh-agent if it's not already running; otherwise get the PID from the PID file.
 if ! [[ -f "${PID_FILE}" ]]; then
-    eval "$(ssh-agent -s -t 20h)"
+    eval "$(ssh-agent -s)"
     echo "${SSH_AGENT_PID}" > "${PID_FILE}"
     chmod 600 "${PID_FILE}"
 else
